4de9f01c57db03a555b907d082b06d72d1e726d2
EddEdw <hupka.adrian@gmail.com>
2017-04-14 21:55:48 +0200

    inital commit

---
+++ SerialSend.ino
@@ -0,0 +1,42 @@
+#include <serialsend.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+//#ifdef MEGA
+    void loop() {
+      // put your main code here, to run repeatedly:
+      //SerialSend::transferInt(100);
+
+      Serial.println("ADRIAN");
+       package paket;
+        paket = SerialSend::receive();
+        if(paket.type == 'i') {
+          Serial.println(paket.i);
+        } else if(paket.type == 'f') {
+          Serial.println(paket.f);
+        } else if(paket.type == 's') {
+          Serial.println(paket.s);
+        }
+        delay(500);
+    }
+//#elif UNO
+//    void loop() {
+//      // put your main code here, to run repeatedly:
+//      SerialSend::transferInt(100);
+//
+//      delay(1000);
+//
+//      SerialSend::transferFloat(123.8912712);
+//
+//      delay(1000);
+//
+//      SerialSend::transferString("Hallo hier ihr!");
+//
+//      delay(1000);
+//    }
+//#endif
+
+
---
+++ serialsend.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace SerialSend {
+
+    struct package {
+        char type = ' ';
+        float f = 0;
+        int i = 0;
+        String s = "";
+    };
+
+    float ten_shift = 10000.0;
+
+    void transferFloat(float f) {
+        
+        int sendVal = f * ten_shift;
+
+        byte * b = (byte *) &sendVal;
+        Serial.print("f:");
+        Serial.write(b[0]);
+        Serial.write(b[1]);
+        Serial.write(b[2]);
+        Serial.write(b[3]);
+        delay(100);
+    }
+
+    void transferInt(int i) {
+        byte * b = (byte *) &i;
+        Serial.print("i:");
+        Serial.write(b[0]);
+        Serial.write(b[1]);
+        Serial.write(b[2]);
+        Serial.write(b[3]);
+        delay(100);
+    }
+
+    void transferString(String str) {
+        // Length (with one extra character for the null terminator)
+        int str_len = str.length() + 1;
+
+        // Prepare the character array (the buffer) 
+        char char_arr[str_len];
+
+        // Copy it over 
+        str.toCharArray(char_arr, str_len);
+
+        Serial.print("s:");
+
+        // transfer string length
+        byte * b = (byte *) &str_len;
+        Serial.write(b[0]);
+        Serial.write(b[1]);
+        Serial.write(b[2]);
+        Serial.write(b[3]);
+
+        Serial.print(":");
+
+        for (int i=0; i<str_len; i++) {
+            Serial.write(char_arr[i]);
+        }
+
+        delay(100);
+    }
+
+    package receive() {
+        package ret_pack;
+        
+        if(Serial.available() > 0) {
+            char inByte = Serial.read();
+            if(inByte == 'f') {
+                // we expect data with this format f:XXXX
+                ret_pack.type = 'f';
+                
+                Serial.read(); // discard ':'
+
+                byte indata[4] = { 0 };
+                indata[0] = Serial.read();
+                indata[1] = Serial.read();
+                indata[2] = Serial.read();
+                indata[3] = Serial.read();
+
+                int g;
+                memcpy(&g, &indata, sizeof(g));
+                ret_pack.f =  g / ten_shift;
+            } else if(inByte == 'i') {
+                // we expect data with this format i:XXXX
+                ret_pack.type = 'i';
+                
+                Serial.read(); // discard ':'
+
+                byte indata[4] = { 0 };
+                indata[0] = Serial.read();
+                indata[1] = Serial.read();
+                indata[2] = Serial.read();
+                indata[3] = Serial.read();
+
+                int g;
+                memcpy(&g, &indata, sizeof(g));
+                ret_pack.i = g;
+            } else if(inByte == 's') {
+                // we expect data with this format s:XXXX
+                ret_pack.type = 's';
+                
+                Serial.read(); // discard ':'
+
+                // read out string length
+                byte indata[4] = { 0 };
+                indata[0] = Serial.read();
+                indata[1] = Serial.read();
+                indata[2] = Serial.read();
+                indata[3] = Serial.read();
+
+                int str_len;
+                memcpy(&str_len, &indata, sizeof(str_len));
+
+                // rebuild string
+                    Serial.read(); // discard ':'
+                // 1. Prepare the character array (the buffer) 
+                char char_arr[str_len];
+                Serial.readBytes(char_arr, str_len);
+                String str(char_arr);
+
+                ret_pack.s = str;
+            } 
+        }
+
+        return ret_pack;
+    }
+}
